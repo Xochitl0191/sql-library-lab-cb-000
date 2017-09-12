8d9144603a24776c05257f1e0e9ca1d8c610e28b
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-12 19:14:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,13 +21,16 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN subgenres
+  ON authors.id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  ""
 end
