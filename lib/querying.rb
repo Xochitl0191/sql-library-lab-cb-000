4bba56a067316e9dfb3f4c7c31e3a8de7d0887ab
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-12 19:12:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,19 +1,23 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year 
+  "SELECT title, year
   FROM books
   WHERE series_id = 1
   ORDER BY year ASC;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto 
+  "SELECT name, motto
   FROM characters
   ORDER BY length(motto) DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT species, COUNT(species) AS total
+  FROM  characters
+  GROUP BY species
+  ORDER BY total DESC
+  LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
