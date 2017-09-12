6af4f789c65a664f20184c38964ad7ef89bc3996
Xochitl Fierros <xochitlfm@gmail.com>
2017-09-12 19:14:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,9 +28,23 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  ""
+  "SELECT title
+  FROM series
+  INNER JOIN character_books
+  ON series.id = book_id
+  INNER JOIN characters
+  ON character_id = characters.id
+  GROUP BY title
+  HAVING COUNT(species = 'human')
+  ORDER BY species DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  ""
+  "SELECT name, COUNT(book_id) AS count
+  FROM characters
+  INNER JOIN character_books
+  ON characters.id = character_books.character_id
+  GROUP BY name
+  ORDER BY count DESC, name ASC;"
 end
